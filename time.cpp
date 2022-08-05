9ad05aaac16a4ae903fbe8208bc32ad1b11abf3e
mohdsuhail2002 <110692329+mohdsuhail2002@users.noreply.github.com>
2022-08-06 00:30:11 +0530

    Add files via upload

---
+++ time.cpp
@@ -0,0 +1,10 @@
+#include<stdio.h>
+int main()
+{
+    int H=0,M=0;
+    printf("enter the time");
+    scanf("%d  %d",&H,&M);
+    printf("%d hour and %d minute");
+    printf("\n");
+    return 0;
+}
